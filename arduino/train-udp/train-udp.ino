59cee8780d022a213665e2bafa104fa747bfa330
jack-borg <jacob.fam.borg@gmail.com>
2019-04-24 14:18:55 +0200

    more content

--- arduino/train-udp/train-udp.ino
+++ arduino/train-udp/train-udp.ino
@@ -1,13 +1,17 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
-/*const char* ssid = "RAILNET";
-const char* password = "it-seminar-2019";*/
+const char* ssid = "RAILNET";
+const char* password = "it-seminar-2019";
 
-const char* ssid = "Das phone";
-const char* password = "wwgu1535";
+/*const char* ssid = "Das phone";
+const char* password = "wwgu1535";*/
+
+/*const char* ssid     = "AndroidAP";
+const char* password = "jjwb5593";*/
 
 #define MOTOR_PWM_PIN D1
+#define HORN_PIN D2
 #define MOTOR_DIR_PIN D3
 #define HALL_SENSOR_A_PIN D5  
 #define HALL_SENSOR_B_PIN D6
@@ -19,6 +23,8 @@ const char* password = "wwgu1535";
 #define TRAIN_INFORMATION 2
 #define TRAIN_CONTROL 3
 #define TRAIN_BRAKE 4
+#define TRAIN_DESTINATION 5
+#define TRAIN_DIRECTION 6
 
 #define I_AM_A_TRAIN 1
 #define I_AM_A_LEFT_SWITCH 2
@@ -39,10 +45,17 @@ int currentDirection = 0;
 const int minSpeedEquivalent = 512;
 const int maxSpeedEquivalent = 800;
 const int maxSpeed = 200;
-int speed;
+int speed = 0;
 
 bool atStation = false;
-int magnetsPassed = 0;
+int magnetsPassed = 2;
+int tripMagnet = 0;
+bool onTrip = false;
+bool stopAtStation = true;
+
+int freq = 1000;
+int horn_channel = 0;
+int resolution = 8;
 
 bool onMagnet = false;
 unsigned long onMagnetTime = 0;
@@ -59,6 +72,7 @@ void setup() {
   pinMode(HALL_SENSOR_B_PIN, INPUT_PULLUP);
   pinMode(ULTRA_SOUND_ECHO, INPUT);
   pinMode(ULTRA_SOUND_TRIGG, OUTPUT);
+  pinMode(HORN_PIN, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(HALL_SENSOR_A_PIN), onHallA, FALLING);
   attachInterrupt(digitalPinToInterrupt(HALL_SENSOR_B_PIN), onHallB, FALLING);
 
@@ -76,6 +90,9 @@ void setup() {
   Udp.begin(localUdpPort);
   Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), localUdpPort);
   digitalWrite(LED_PIN, LOW);
+
+  /*ledcSetup(horn_channel, freq, resolution);
+  ledcAttachPin(HORN_PIN, horn_channel);*/
   }
 
 
@@ -85,6 +102,7 @@ void loop()
   int packetSize = Udp.parsePacket();
   if (packetSize)
   {
+    Serial.println("recived");
     remoteIP = Udp.remoteIP();
     remotePort = Udp.remotePort();
     // receive incoming UDP packets
@@ -98,8 +116,19 @@ void loop()
         applySpeed(speed);
         }
       else if (incomingPacket[1] == TRAIN_BRAKE) {
-        applySpeed(0);
+        speed = 0;
+        applySpeed(speed);
         sendTrainInformation(0, 0, 0, 1);
+        }
+      else if (incomingPacket[1] == TRAIN_DESTINATION) {
+        /*speed = readInt(incomingPacket, 2);
+        Serial.printf("Speed: %d\n", speed);
+        applySpeed(speed);*/
+        tripMagnet = readInt(incomingPacket, 2);
+        }
+      else if (incomingPacket[1] == TRAIN_DIRECTION){
+        currentDirection = readInt(incomingPacket,2);
+        applySpeed(speed);
         }
       else {
         incomingPacket[len] = 0;
@@ -113,18 +142,8 @@ void loop()
   }
 
   void US(){
-    if(!atStation && magnetsPassed >= 2){
-      long duration, distance;
-      digitalWrite(ULTRA_SOUND_TRIGG, LOW);
-      delayMicroseconds(2);
-      digitalWrite(ULTRA_SOUND_TRIGG, HIGH);
-      delayMicroseconds(10);
-      digitalWrite(ULTRA_SOUND_TRIGG, LOW);
-      duration = pulseIn(ULTRA_SOUND_ECHO, HIGH);
-      distance = (duration/2) / 29.1;
-      Serial.println(distance);
-      if(distance < 20 ){
-      //if(checkDistance() < 20 ){
+    if(!atStation && magnetsPassed >= 2 && stopAtStation){
+      if(checkDistance() < 20 ){
         trainStopTime = millis();
         Serial.print("   time: ");
         Serial.println(trainStopTime);
@@ -173,10 +192,6 @@ int writeInt(char buffer[], int index, int value) {
   return index + 4;
   }
 
-  void reverseDirection(){
-    currentDirection = (currentDirection+1)%2;
-  }
-
 void applySpeed(int speedCmd) {
   int d = (maxSpeedEquivalent - minSpeedEquivalent)/maxSpeed;
   int speedEquivalent = minSpeedEquivalent + speedCmd*d;
@@ -200,7 +215,18 @@ void onHallB() {
   onMagnetTime = millis() - onMagnetTime;
   sendTrainInformation((5000/onMagnetTime), 0, 0, 1);
   magnetsPassed++;
+  if(onTrip){
+    tripMagnet--;
+    if(tripMagnet == 0){
+      speed = 0;
+      applySpeed(speed);
+      onTrip = false;
+    }
   }
+  analogWrite(HORN_PIN, 500);
+  /*ledcWriteTone(horn_channel,1000);
+  ledcWrite(horn_channel, freq);*/
+}
 
 void sendTrainInformation(int speed, int trackId, int distanceToLight, int light) {
   outgoingPacket[0] = 15;
